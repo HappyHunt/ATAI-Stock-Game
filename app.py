b53aa4f72988ab09dab23ab9b653b39faf97a117
Johny <johny.weee@protonmail.com>
2023-03-04 13:51:13 +0100

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import uvicorn
 from fastapi import FastAPI
 from views import sample
 from init import Init
+
 
 app = FastAPI()
 app.include_router(sample.router)
@@ -10,7 +12,4 @@ db = init.get_db()
 kucoin = init.get_kucoin_client()
 
 if __name__ == '__main__':
-    import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
-
-
